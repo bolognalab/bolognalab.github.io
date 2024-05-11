bf6da6b66195b901d6ac3ff5062bc740455d917d
naspitha <naspitha@users.noreply.github.com>
2024-05-11 02:45:41 +0200

    change python file

--- files/csv2json.py
+++ files/csv2json.py
@@ -16,28 +16,23 @@ with open(csv_file, mode='r', encoding='utf-8') as csv_file:
     json_list = []
     for r, row_data in enumerate(csvfiledata):
         fixed_data = {'short-event-name' if k == '\ufeffshort-event-name' else k:v for k,v in row_data.items()}
-        # word = fixed_data
-        # s = "-".join([word["Kana"], word["English"][:15], word["Category"]])
-        # swaps = [("{", "X"), ("}", "X"), ("/", "L")]
-        # for old, new in swaps:
-        #     s = s.replace(old, new)
-        # fixed_data['id']=s
-        # print(fixed_data)
+
         json_list.append(fixed_data)
         names_list.append(fixed_data["id"])
         titles[fixed_data["id"]]=fixed_data["long-event-name"]
-        
+
 def makeMD(names):
     for name in names:
         fname = "../calendar-events/" + name + ".md"
         print(fname, os.path.exists(fname))
         if not os.path.exists(fname):
             with open(fname, 'w', encoding="utf-8") as file:
-                file.write("# " + titles[name] + '  ')
+                file.write("# " + titles[name] + '  \n')
+                file.write("## Ort (z.B, Senatsaal)  \n##### Adresse und Raum \n--- \n### Art der Veranstaltung (z.B. Workshop) \nLeitung: \ \n**Name 1**  \n##### Einrichtung 1\ ")
                 pass
         
-# print(json_list)
-# with open(json_file, 'w') as json_file:
-#     json_file.write(json.dumps(json_list, indent=4))
+def updateJSON(json_list):
+    with open(json_file, 'w') as json_file:
+        json_file.write(json.dumps(json_list, indent=4))
 
     
